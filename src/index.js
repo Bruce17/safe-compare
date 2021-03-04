41c43da580b80e3aacf7a2ff020dcdc3b257a606
Michael Raith <mraith@gmail.com>
2021-03-04 10:27:47 +0100

    set Node.js min to v6.9 and remove unused code

--- src/index.js
+++ src/index.js
@@ -6,37 +6,6 @@
 'use strict';
 
 var crypto = require('crypto');
-var bufferAllocUnsafe = require('buffer-alloc-unsafe');
-
-
-/**
- * Do a constant time string comparison. Always compare the complete strings
- * against each other to get a constant time. This method does not short-cut
- * if the two string's length differs.
- *
- * @param {string} a
- * @param {string} b
- *
- * @return {boolean}
- */
-var safeCompare = function safeCompare(a, b) {
-    var strA = String(a);
-    var strB = String(b);
-    var lenA = strA.length;
-    var result = 0;
-
-    if (lenA !== strB.length) {
-        strB = strA;
-        result = 1;
-    }
-
-    for (var i = 0; i < lenA; i++) {
-        result |= (strA.charCodeAt(i) ^ strB.charCodeAt(i));
-    }
-
-    return result === 0;
-};
-
 
 /**
  * Call native "crypto.timingSafeEqual" methods.
@@ -50,7 +19,7 @@ var safeCompare = function safeCompare(a, b) {
  *
  * @return {boolean}
  */
-var nativeTimingSafeEqual = function nativeTimingSafeEqual(a, b) {
+module.exports = function nativeTimingSafeEqual(a, b) {
     var strA = String(a);
     var strB = String(b);
     var aLen = Buffer.byteLength(strA);
@@ -59,17 +28,10 @@ var nativeTimingSafeEqual = function nativeTimingSafeEqual(a, b) {
     // Always use length of a to avoid leaking the length. Even if this is a
     // false positive because one is a prefix of the other, the explicit length
     // check at the end will catch that.
-    var bufA = bufferAllocUnsafe(aLen);
+    var bufA = Buffer.allocUnsafe(aLen);
     bufA.write(strA);
-    var bufB = bufferAllocUnsafe(aLen);
+    var bufB = Buffer.allocUnsafe(aLen);
     bufB.write(strB);
 
     return crypto.timingSafeEqual(bufA, bufB) && aLen === bLen;
 };
-
-
-module.exports = (
-    typeof crypto.timingSafeEqual !== 'undefined' ?
-        nativeTimingSafeEqual :
-        safeCompare
-);
